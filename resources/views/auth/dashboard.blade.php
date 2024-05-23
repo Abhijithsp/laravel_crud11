a5efcbb972002324e14cd33792d50af766022dc5
Abhijith <abhijithsp1@gmail.com>
2024-05-23 16:00:42 +0530

    minor change in navbar added telescope for debugging

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,3 +1,3 @@
 <x-layout>
-    <h1>Welocme</h1>
+    <h1>Welocme Dashboard</h1>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -21,35 +21,6 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 
 
-//use App\Http\Controllers\LoginController;
-//use App\Http\Controllers\PostsController;
-//use App\Http\Controllers\RegisterController;
-//use Illuminate\Support\Facades\Route;
-//
-//Route::get('/', function () {
-//    return view('auth.login');
-//});
-//
-//Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
-//Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
-//
-//Route::middleware('auth')->group(function () {
-//    Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
-//    Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
-//    Route::get('posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
-//    Route::put('posts/{post}', [PostsController::class, 'update'])->name('posts.update');
-//    Route::delete('posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
-//    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-//    Route::get('/dashboard', [LoginController::class, 'dashboard'])->name('dashboard');
-//});
-//
-//Route::middleware('guest')->group(function () {
-//    Route::get('/register', [RegisterController::class, 'register'])->name('register');
-//    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-//
-//    Route::get('/login', [LoginController::class, 'login'])->name('login');
-//    Route::post('/login', [LoginController::class, 'store'])->name('login.store');
-//});
 
 
 
